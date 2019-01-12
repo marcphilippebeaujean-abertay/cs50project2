f5c177b667ea616d3a37eadf7d0f5f21dac9102f
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-01-12 17:39:47 +0000

    now transpiling to sass via webpack

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,6 @@
 import x from "./testmodule";
+import "../style/style.scss";
+
 
 const z = 23;
 console.log(`${x} hello world Variable z is ${z}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+    filename: 'style.css'
+})
 
 module.exports = {
   entry: ['babel-polyfill', './static/js/index.js'],
   output: {
       path: path.resolve(__dirname, 'static/dist'),
-      filename: "js/bundle.js"
+      filename: "bundle.js"
   },
   devServer: {
       contentBase: './static/dist'
@@ -17,7 +22,19 @@ module.exports = {
               use: {
                   loader: 'babel-loader'
               }
+          },
+          {
+              test: /\.scss$/,
+              use: extractPlugin.extract({
+                  use: [
+                      'css-loader',
+                      'sass-loader'
+                  ]
+              })
           }
       ]
-  }
+  },
+  plugins: [
+      extractPlugin
+  ]
 };
