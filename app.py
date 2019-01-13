ca36167e46959bc277d35451f00e3ed7bec68e79
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-01-13 13:10:51 +0000

    once again trying to deploy

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    #return render_template("home.html")
-    return "hello world"
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
