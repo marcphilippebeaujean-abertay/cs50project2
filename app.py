ef5a1d678abffceae5622a5d5b884ddda251b205
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-01-13 13:09:08 +0000

    added procfile and other dependancies for heroku deployment

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("home.html")
+    #return render_template("home.html")
+    return "hello world"
 
 
 if __name__ == '__main__':
