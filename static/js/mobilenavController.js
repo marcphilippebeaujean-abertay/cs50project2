b9121e991a99c0d3675e981dd611f2a17476cc52
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2019-02-02 14:44:03 +0000

    got the hamburger bar to toggle between the arrow

--- static/js/mobilenavController.js
+++ static/js/mobilenavController.js
@@ -7,6 +7,7 @@ export default class MobileNavController {
         this.showingChat = true;
 
         this.onScreenResizedToPhoneView = this.onScreenResizedToPhoneView.bind(this);
+        this.onScreenReducedToPhoneView = this.onScreenReducedToPhoneView.bind(this);
 
         // Setup media query for user changing view size
         const mediaQuery = window.matchMedia("(min-width: 600px)");
@@ -14,23 +15,41 @@ export default class MobileNavController {
             this.onScreenResizedToPhoneView
         );
 
-        document.getElementById('bars-nav').addEventListener(
-            'click',
-            () => {
+        const mediaShrinkQuery = window.matchMedia("(max-width: 600px)");
+        mediaQuery.addListener(
+            this.onScreenReducedToPhoneView
+        );
+
+        this.phoneBarsNav = document.getElementById("hamburger-bar");
+        this.arrowPhoneNav = document.getElementById("back-arrow");
+        let toggleChat = () => {
             this.showingChat = !this.showingChat;
-            if(this.showingChat){
-                if(!this.chatViewList.classList.contains(hideElemClass)) {
+            if (this.showingChat) {
+                if (!this.chatViewList.classList.contains(hideElemClass)) {
                     this.chatViewList.classList.add(hideElemClass);
                 }
                 this.msgList.style.display = 'inline';
-            }else{
-                if(this.chatViewList.classList.contains(hideElemClass)) {
+                this.phoneBarsNav.style.display = 'block';
+                this.arrowPhoneNav.style.display = 'none';
+            } else {
+                if (this.chatViewList.classList.contains(hideElemClass)) {
                     this.chatViewList.classList.remove(hideElemClass);
                 }
                 this.msgList.style.display = 'none';
+                this.phoneBarsNav.style.display = 'none';
+                this.arrowPhoneNav.style.display = 'block';
             }
-        });
+        };
+        this.arrowPhoneNav.addEventListener(
+            'click',
+            toggleChat
+        );
+        this.phoneBarsNav.addEventListener(
+            'click',
+            toggleChat
+        );
     }
+
     onScreenResizedToPhoneView(e){
         if(e.matches){
             if(!this.chatViewList.classList.contains(hideElemClass)) {
@@ -38,6 +57,12 @@ export default class MobileNavController {
             }
             this.msgList.style.display = 'inline';
             this.showingChat = true;
+        }
+    }
+    onScreenReducedToPhoneView(e){
+        if(e.matches){
+            this.phoneBarsNav.style.display = 'block';
+            this.arrowPhoneNav.style.display = 'none';
         }
     }
 }
